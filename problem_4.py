d6f5425eaa9db8c13a2af8321a9c76b6a98bdb27
Jason Zheng <jason1996zheng@gmail.com>
2020-06-25 09:27:59 -0500

    Create problem_4.py

---
+++ problem_4.py
@@ -0,0 +1,38 @@
+#Problem 4: Active Directory
+
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+        
+def is_user_in_group(user, group):
+    in_group = False
+    
+    for users in group.get_users():
+        if users == user: 
+            in_group = True
+            return in_group
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    return in_group
